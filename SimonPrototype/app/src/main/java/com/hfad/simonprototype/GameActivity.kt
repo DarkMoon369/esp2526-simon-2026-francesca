fdd1e2b39d149e20abe1aa68cc7463becf5442e9
vfran <francesca.vavasori@studenti.unipd.it>
2026-05-05 17:28:37 +0200

    Creata GameActivity e aggiunto pulsante per avviare il gioco dalla Lista delle Partite

--- SimonPrototype/app/src/main/java/com/hfad/simonprototype/GameActivity.kt
+++ SimonPrototype/app/src/main/java/com/hfad/simonprototype/GameActivity.kt
@@ -1,5 +1,8 @@
 package com.hfad.simonprototype
 
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
 class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
